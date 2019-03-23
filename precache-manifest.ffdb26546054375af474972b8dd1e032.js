222fb1555a14bbe35ea5a51797149451988f1778
Gabriela <balicas.gb@gmail.com>
2019-03-22 19:10:14 -0700

    Updates

---
+++ precache-manifest.ffdb26546054375af474972b8dd1e032.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9f5cc7646ca489e9004",
+    "url": "/flower-puzzle/static/css/main.d5517436.chunk.css"
+  },
+  {
+    "revision": "e9f5cc7646ca489e9004",
+    "url": "/flower-puzzle/static/js/main.e9f5cc76.chunk.js"
+  },
+  {
+    "revision": "ef7273fec328245b23a3",
+    "url": "/flower-puzzle/static/js/1.ef7273fe.chunk.js"
+  },
+  {
+    "revision": "aa0f4685cc12155a8072",
+    "url": "/flower-puzzle/static/js/runtime~main.aa0f4685.js"
+  },
+  {
+    "revision": "e286f47253e0e49031fc47436868f8f6",
+    "url": "/flower-puzzle/index.html"
+  }
+];
